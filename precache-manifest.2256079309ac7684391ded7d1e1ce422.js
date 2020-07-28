c99a60f20b116db3ec94b12224248f55df7e29d9
Simon Poissant <simon.poissant@gmail.com>
2020-07-28 00:31:35 -0400

    Updates

---
+++ precache-manifest.2256079309ac7684391ded7d1e1ce422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75b60a662340e49152daf08ebec9328",
+    "url": "/wh40k-core-rules/index.html"
+  },
+  {
+    "revision": "0188e9fbb2764970ee61",
+    "url": "/wh40k-core-rules/static/css/main.1a2da361.chunk.css"
+  },
+  {
+    "revision": "da3af3536b7e36d91142",
+    "url": "/wh40k-core-rules/static/js/2.cea47c21.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/wh40k-core-rules/static/js/2.cea47c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0188e9fbb2764970ee61",
+    "url": "/wh40k-core-rules/static/js/main.a9d78f32.chunk.js"
+  },
+  {
+    "revision": "613caf11f73c4a963810",
+    "url": "/wh40k-core-rules/static/js/runtime-main.fd5dd5d3.js"
+  }
+]);
