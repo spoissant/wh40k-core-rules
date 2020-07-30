77aba789b6f2896bb52e58fa59db487d2553da14
Simon Poissant <simon.poissant@gmail.com>
2020-07-30 13:01:45 -0400

    Updates

--- precache-manifest.a1205e3ecaac2f5043f3f7372ce475cf.js
+++ precache-manifest.a1205e3ecaac2f5043f3f7372ce475cf.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df90a1966ffd6b4d5f2682595fe07e12",
+    "revision": "d0668e8f2aab4b0e909e2f9b3633e7d6",
     "url": "/wh40k-core-rules/index.html"
   },
   {
-    "revision": "a7b8338be055cd70a6cc",
+    "revision": "215d15dde396f071df72",
     "url": "/wh40k-core-rules/static/css/main.e6a02286.chunk.css"
   },
   {
-    "revision": "f2571a93d920cad63331",
-    "url": "/wh40k-core-rules/static/js/2.3705855a.chunk.js"
+    "revision": "444ea642074ab3f93bb7",
+    "url": "/wh40k-core-rules/static/js/2.def452ce.chunk.js"
   },
   {
     "revision": "54fd6816419f7824e2804745930c9117",
-    "url": "/wh40k-core-rules/static/js/2.3705855a.chunk.js.LICENSE.txt"
+    "url": "/wh40k-core-rules/static/js/2.def452ce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a7b8338be055cd70a6cc",
-    "url": "/wh40k-core-rules/static/js/main.05749184.chunk.js"
+    "revision": "215d15dde396f071df72",
+    "url": "/wh40k-core-rules/static/js/main.8ab47137.chunk.js"
   },
   {
     "revision": "613caf11f73c4a963810",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wh40k-core-rules/precache-manifest.8c7e1b4a4c5fac83a5fdce138c807381.js"
+  "/wh40k-core-rules/precache-manifest.a1205e3ecaac2f5043f3f7372ce475cf.js"
 );
 
 self.addEventListener('message', (event) => {
