584a33da77171a00cc5fa124abbe5e7081cea9ab
Simon Poissant <simon.poissant@gmail.com>
2020-09-15 23:02:24 -0400

    Updates

---
+++ precache-manifest.ff297b0bc4e336e2add0e6aad02ab24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c167851d03f99c7b72cf3d3e803aef",
+    "url": "/wh40k-core-rules/index.html"
+  },
+  {
+    "revision": "9e93edd06edadcc35ddb",
+    "url": "/wh40k-core-rules/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c57c52c5b4d74b144e7b",
+    "url": "/wh40k-core-rules/static/js/2.17020127.chunk.js"
+  },
+  {
+    "revision": "d455b55662ec59e842e850740eeaf3d2",
+    "url": "/wh40k-core-rules/static/js/2.17020127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e93edd06edadcc35ddb",
+    "url": "/wh40k-core-rules/static/js/main.2f6025c1.chunk.js"
+  },
+  {
+    "revision": "613caf11f73c4a963810",
+    "url": "/wh40k-core-rules/static/js/runtime-main.fd5dd5d3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wh40k-core-rules/precache-manifest.3a9ae971f7d3dae9b2580f478717b075.js"
+  "/wh40k-core-rules/precache-manifest.ff297b0bc4e336e2add0e6aad02ab24e.js"
 );
 
 self.addEventListener('message', (event) => {
